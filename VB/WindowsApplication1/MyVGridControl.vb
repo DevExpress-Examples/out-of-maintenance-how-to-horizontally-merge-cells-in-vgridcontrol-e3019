1b7ada144d541bc19ea0061dd7662ce95efd2e1a
DevExpressExampleBot <fakeEmail>
2019-08-19 21:31:13 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/MyVGridControl.vb
+++ VB/WindowsApplication1/MyVGridControl.vb
@@ -3,15 +3,15 @@ Imports DevExpress.XtraVerticalGrid
 Imports DevExpress.XtraVerticalGrid.Painters
 
 Namespace WindowsApplication1
-	Public Class MyVGridControl
-		Inherits VGridControl
+    Public Class MyVGridControl
+        Inherits VGridControl
 
-		Public Sub New()
+        Public Sub New()
 
-		End Sub
+        End Sub
 
-		Protected Overrides Function CreatePainterCore(ByVal eventHelper As PaintEventHelper) As VGridPainter
-			Return New MyVGridPainter(eventHelper)
-		End Function
-	End Class
+        Protected Overrides Function CreatePainterCore(ByVal eventHelper As PaintEventHelper) As VGridPainter
+            Return New MyVGridPainter(eventHelper)
+        End Function
+    End Class
 End Namespace
